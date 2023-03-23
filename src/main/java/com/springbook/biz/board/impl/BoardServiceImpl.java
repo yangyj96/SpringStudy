1b2512e5175a14c98a37e56c8b53237478610e70
yangyj96 <yangyj96@naver.com>
2023-03-23 18:29:38 +0900

    Spring study ~p197

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -13,7 +13,12 @@ public class BoardServiceImpl implements BoardService {
 	@Autowired
 	private BoardDAO boardDAO;
 
+
 	public void insertBoard(BoardVO vo) {
+		
+		if(vo.getSeq() == 0) { 
+			throw new IllegalArgumentException("0번 글은 등록 할수 없습니다."); }
+		 
 		boardDAO.insertBoard(vo);
 	}
 
